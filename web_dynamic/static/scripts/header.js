cac5a58e47257eeb2ff0434382611c3329949233
theotsori <theodoretsori@gmail.com>
2023-05-25 16:25:22 +0300

    added search results page and function

--- web_dynamic/static/scripts/header.js
+++ web_dynamic/static/scripts/header.js
@@ -1,3 +1,18 @@
+// Get references to the form and input element
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+// Add an event listener to the form submission
+searchForm.addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  const query = searchInput.value; // Get the entered query
+  const url = `/search?query=${encodeURIComponent(query)}`; // Create the URL with the query parameter
+
+  window.location.href = url; // Redirect to the search URL
+});
+
+
 // select the menu toggle button
 function initializeMenuToggle() {
   var menuToggle = document.querySelector('.menu-toggle');
