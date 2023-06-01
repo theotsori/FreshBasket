cb1806d49d760d97764dca905d658a9b9301a52f
theotsori <theodoretsori@gmail.com>
2023-06-01 10:57:27 +0300

    Minor file arrangement

--- web_static/scripts/header.js
+++
@@ -1,7 +0,0 @@
-// select the menu toggle button
-const menuToggle = document.querySelector('.menu-toggle');
-  const nav = document.querySelector('nav.show-menu');
-
-  menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-  });
