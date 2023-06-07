c8c117aab69bc112b53209767e454fbaace12699
theotsori <theodoretsori@gmail.com>
2023-06-07 12:09:55 +0300

    upadated config file and requirements

---
+++ config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import os
+
+# Flask configuration
+SECRET_KEY = os.environ.get('APP_SECRET_KEY')
+
+# MySQL configuration
+DB_HOST = os.environ.get('DB_HOST')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_DATABASE = os.environ.get('DB_DATABASE')
+
+# MySQL connection config
+DB_CONFIG = {
+    'host': DB_HOST,
+    'user': DB_USER,
+    'password': DB_PASSWORD,
+    'database': DB_DATABASE
+}
